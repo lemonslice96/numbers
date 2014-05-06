3681bc5c7d9d8d08d075ca0a19ddcad89d5ed29f
lemonslice96 <lemonslice96@gmail.com>
2014-05-06 10:09:52 -0400

    xvndrghd

---
+++ Numbers/ConsoleApplication1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        public static void feet(int inches)
+        {
+            Console.WriteLine("{0} feet {1} inches")
+        }
+        static void Main(string[] args)
+        {
+            int inches = 139;
+            feet(inches);
+            
+        }
+    }
+}
---
+++ Numbers/Numbers/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        public static void sum(int a, int b)
+        {
+            Console.WriteLine(a + b);
+        }
+        public static void difference(int a, int b)
+        {
+            Console.WriteLine(a - b);
+        }
+        public static int product(int a, int b)
+        {
+            return a * b;
+        }
+        static void Main(string[] args)
+        {
+            int e = 100, r = 50;
+            sum(e, r);
+            difference(e, r);
+            int result = product(e, r);
+            Console.WriteLine(result);
+        }
+    }
+}
