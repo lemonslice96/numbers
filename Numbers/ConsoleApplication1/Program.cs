f903824db130cfc5f10039964709490045f32c8e
lemonslice96 <lemonslice96@gmail.com>
2014-05-07 10:08:58 -0400

    fdghdhg

--- Numbers/ConsoleApplication1/Program.cs
+++ Numbers/ConsoleApplication1/Program.cs
@@ -9,12 +9,17 @@ namespace ConsoleApplication1
     {
         public static void feet(int inches)
         {
-            Console.WriteLine("{0} feet {1} inches")
+            Console.WriteLine("{0} feet {1} inches", inches / 12, inches % 12);
+        }
+        public static void yards(int inches)
+        {
+            Console.WriteLine("{0} yards and {1} feet and {2} inches", inches / 12 / 3, inches / 12 % 3, inches % 12);
         }
         static void Main(string[] args)
         {
             int inches = 139;
             feet(inches);
+            yards(inches);
             
         }
     }
---
+++ Numbers/monogram/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace monogram
+{
+    class Program
+    {
+        public void DisplayMonogram(char a, char b, char c, char d, char e, char f)
+        {
+            Console.WriteLine("** " + a + ". " + b + ". " + c + ". **");
+            Console.WriteLine("** " + d + ". " + e + ". " + f + ". **");
+ 
+        }
+        static void Main()
+        {
+            char a = 'J'; char b = 'A'; char c = 'L'; char d = 'R'; char e = 'J';  char f = 'C';
+            DisplayMonogram(a, b, c, d, e, f);
+
+        }
+    }
+}
