bcffebd64a2bd85feac6403489c46688536cf96a
lemonslice96 <lemonslice96@gmail.com>
2014-05-08 10:10:47 -0400

    vxvbcvbn

--- Numbers/reverse3.cs/Program.cs
+++ Numbers/reverse3.cs/Program.cs
@@ -3,18 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace ConsoleApplication2
+namespace reverse3.cs
 {
     class Program
     {
-        public static void square(int a)
-        {
-            Console.WriteLine(a * a); 
-        }
         static void Main(string[] args)
         {
-            int a;
-            
+
         }
     }
 }
--- Numbers/square.cs/Program.cs
+++ Numbers/square.cs/Program.cs
@@ -7,14 +7,17 @@ namespace ConsoleApplication2
 {
     class Program
     {
-        public static void square(int a)
+        public static void square(ref int a)
         {
-            Console.WriteLine(a * a); 
+            Console.WriteLine(a * a);
         }
         static void Main(string[] args)
         {
             int a;
-            
+            Console.WriteLine("Number?");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(a);
+            square(ref a);
         }
     }
 }
